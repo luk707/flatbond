6e9a4c9ae7c77c7b72c3bbaa05a192f3613762de
Luke Harris <lukeharris01170@gmail.com>
2019-01-21 12:23:40 +0000

    Style slider component

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -21,11 +21,47 @@ export default {
 </script>
 
 <template>
-  <div>
-    <input type="range" :min="min" :max="max" :value="value" @input="handleChange">
+  <div class="slidecontainer">
+    <input class="slider" type="range" :min="min" :max="max" :value="value" @input="handleChange">
   </div>
 </template>
 
-<style>
-</style>
+<style scoped>
+.slidecontainer {
+  width: 100%;
+}
 
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 10px;
+  border-radius: 5px;
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: 0.2s;
+  transition: opacity 0.2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #7b93db;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #7b93db;
+  cursor: pointer;
+}
+</style>

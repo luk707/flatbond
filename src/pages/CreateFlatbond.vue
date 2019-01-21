a423400b16874cea0fbe25eb441b0b74b5c247e1
Luke Harris <lukeharris01170@gmail.com>
2019-01-21 15:55:26 +0000

    Fix bug with membership fee amount and pull data from endpoint

--- src/pages/CreateFlatbond.vue
+++ src/pages/CreateFlatbond.vue
@@ -1,5 +1,6 @@
 <script>
 import { mapActions } from "vuex";
+import axios from "axios";
 
 import constants from "../constants";
 
@@ -19,13 +20,28 @@ export default {
   },
   data() {
     return {
-      loading: true
+      loading: true,
+      error: null,
+      fixed_membership_fee: false,
+      fixed_membership_fee_amount: 0
     };
   },
   created() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 2000);
+    this.loading = true;
+    this.error = null;
+    axios
+      .get(
+        "https://cxynbjn3wf.execute-api.eu-west-2.amazonaws.com/production/config"
+      )
+      .then(response => {
+        this.fixed_membership_fee = response.data.fixed_membership_fee;
+        this.fixed_membership_fee_amount =
+          response.data.fixed_membership_fee_amount / 100;
+        this.loading = false;
+      })
+      .catch(error => {
+        this.error = true;
+      });
   },
   computed: {
     paymentPeriod() {
@@ -38,7 +54,8 @@ export default {
       return this.$store.state.rentRangeMax;
     },
     rentValue() {
-      return this.$store.state.rentValue;
+      // Convert to number
+      return ~~this.$store.state.rentValue;
     },
     prettyRentValue() {
       return scalarFormatter(
@@ -53,8 +70,8 @@ export default {
       } = calculateFlatbondBreakdown({
         rentValue: this.$store.state.rentValue / 100,
         monthly: this.$store.state.paymentPeriod === "Monthly",
-        fixedMembershipFee: false,
-        fixedMembershipFeeAmountL: 0
+        fixedMembershipFee: this.fixed_membership_fee || false,
+        fixedMembershipFeeAmount: this.fixed_membership_fee_amount || 0
       });
       return {
         membership_fee_before_vat: scalarFormatter(
